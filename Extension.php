4f68d15921f801a9f3ba57755b809ed1c13725c3
skinysmn <hid-mail-02@mail.ru>
2016-09-19 16:35:36 +0300

    Add files via upload

---
+++ Extension.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Bolt\Extension\levin\filesize;
+
+use Bolt;
+
+class Extension extends \Bolt\BaseExtension
+{
+    public function getName()
+    {
+        return "filesize";
+    }
+
+    public function initialize()
+    {
+        $this->addTwigFunction('filesize', 'twigFilesize');
+    }
+
+    public function twigFilesize($file)
+    {
+
+ if (file_exists($_SERVER['DOCUMENT_ROOT']."/files/".$file)){
+        $bytes = filesize ($_SERVER['DOCUMENT_ROOT']."/files/".$file);
+        $units = array('б', 'Kб', 'Mб', 'Gб', 'Tб');
+        $bytes = max($bytes, 0);
+        $pow = floor(($bytes?log($bytes):0)/log(1024));
+        $pow = min($pow, count($units)-1);
+        $bytes /= pow(1024, $pow);
+        $size = round($bytes, '2').' '.$units[$pow];
+			return new \Twig_Markup($size, 'UTF-8');
+		}
+	}
+}
