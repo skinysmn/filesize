b5b5e75ed49d3545ac3633143bea7ed8e9a44459
skinysmn <hid-mail-02@mail.ru>
2016-09-19 16:40:40 +0300

    Update Extension.php

--- Extension.php
+++ Extension.php
@@ -21,7 +21,7 @@ class Extension extends \Bolt\BaseExtension
 
  if (file_exists($_SERVER['DOCUMENT_ROOT']."/files/".$file)){
         $bytes = filesize ($_SERVER['DOCUMENT_ROOT']."/files/".$file);
-        $units = array('б', 'Kб', 'Mб', 'Gб', 'Tб');
+        $units = array('b', 'Kb', 'Mb', 'Gb', 'Tb');
         $bytes = max($bytes, 0);
         $pow = floor(($bytes?log($bytes):0)/log(1024));
         $pow = min($pow, count($units)-1);
