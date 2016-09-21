c3951e6ad38534e0bfb8fdd842f32fb777c6e15d
skinysmn <hid-mail-02@mail.ru>
2016-09-21 18:42:55 +0300

    Update and rename Extension.php to src/filesizeExtension.php

---
+++ src/filesizeExtension.php
@@ -0,0 +1,30 @@
+<?php
+namespace Bolt\Extension\levin\filesize;
+
+use Bolt\Extension\SimpleExtension;
+
+class filesizeExtension extends SimpleExtension
+{
+ protected function registerTwigFunctions()
+    {
+        return [
+            'filesize' => 'TwigFilesize',
+        ];
+    }
+
+    public function TwigFilesize($file)
+    {
+
+	if (file_exists($_SERVER['DOCUMENT_ROOT']."/files/".$file)){
+	    $bytes = filesize ($_SERVER['DOCUMENT_ROOT']."/files/".$file);
+	    $units = array('b', 'Kb', 'Mb', 'Gb', 'Tb');
+	    $bytes = max($bytes, 0);
+	    $pow = floor(($bytes?log($bytes):0)/log(1024));
+	    $pow = min($pow, count($units)-1);
+	    $bytes /= pow(1024, $pow);
+	    $size = round($bytes, '2').' '.$units[$pow];
+	    
+	return new \Twig_Markup($size, 'UTF-8');
+	}
+    }
+}
